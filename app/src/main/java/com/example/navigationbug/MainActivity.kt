aa2b8760aba37c64d614a35894fa54f07465d702
CAESAR\zamee <zameelpichen@gmail.com>
2024-01-26 12:30:30 -0500

    better fix

--- app/src/main/java/com/example/navigationbug/MainActivity.kt
+++ app/src/main/java/com/example/navigationbug/MainActivity.kt
@@ -1,27 +1,32 @@
 package com.example.navigationbug
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.FrameLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.navigationbug.ui.theme.NavigationBugDemoTheme
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
-    private val onBackPressedCallback: OnBackPressedCallback =
-        object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                println("onBackPressedCallback")
-            }
+
+    private val onBackPressedCallback: OnBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            println("hahaha")
         }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
         onBackPressedDispatcher.addCallback(this, onBackPressedCallback) // navigation breaker
 
@@ -32,13 +37,40 @@ class MainActivity : AppCompatActivity() {
                 FrameLayout.LayoutParams.MATCH_PARENT
             )
         }
-
+        containerId = container.id
         setContentView(container)
+
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .add(container.id, MainFragment())
                 .commit()
         }
+
+    }
+    private var containerId: Int = 0
+
+    override fun onBackPressed() {
+        val mainFragment = supportFragmentManager.findFragmentById(containerId)
+        if (mainFragment is MainFragment) {
+            val navController = mainFragment.navController
+            if (navController != null && navController.navigateUp()) {
+                // Navigation handled by NavController
+                return
+            }
+        }
+        super.onBackPressed()
+    }
+    override fun onResume() {
+        super.onResume()
+        // Re-enable the callback
+        onBackPressedCallback.isEnabled = true
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        // Optionally disable the callback
+        onBackPressedCallback.isEnabled = false
     }
 }
--- app/src/main/java/com/example/navigationbug/MainFragment.kt
+++ app/src/main/java/com/example/navigationbug/MainFragment.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.compose.BackHandler
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.navigationbug.ui.theme.NavigationBugDemoTheme
 
 class MainFragment : Fragment() {
+    private var _navController: NavController? = null
+    val navController: NavController?
+        get() = _navController
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +27,7 @@ class MainFragment : Fragment() {
             setContent {
                 NavigationBugDemoTheme {
                     val navController = rememberNavController()
+                    _navController = navController
 
                     NavHost(
                         navController = navController,
@@ -28,10 +35,16 @@ class MainFragment : Fragment() {
                     ) {
                         appNavGraph(
                             navController = navController,
+
                         )
                     }
                 }
             }
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _navController = null
     }
 }
